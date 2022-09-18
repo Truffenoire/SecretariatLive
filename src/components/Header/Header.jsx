c8f716a3e6e5b3e6bebb367a22f0966ad3dbbf97
GuillaumeM <Guiwilh@gmail.com>
2022-09-18 11:32:46 +0200

    effect anim

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 // import { Link } from 'react-router-dom'
 import { HashLink as Link } from 'react-router-hash-link';
 import logo from '../../imageImport/logoPetit.png'
+
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 
 
 const Header = ( {showMenu, setShowMenu, accueil, setAccueil, service, setService, tarifs, setTarifs, contact, setContact, faq, setFaq, sousMenu, setSousMenu} ) => {
@@ -69,31 +75,32 @@ const Header = ( {showMenu, setShowMenu, accueil, setAccueil, service, setServic
         console.log(location[3]);
     // }
 
+        
+
     return (
         <div className='header'>
             <div className='logo'>
                 <Link id={'ancre'} to={"/SecretariatLive"}><img src={logo} alt="Logo secretariat Live" /></Link>
             </div>
-            <nav className={`navBar ${showMenu ? "showNav" : "hideNav"}`}>
-                <ul>
-                    <Link className={accueil} onMouseEnter={handleClose} onClick={handleAccueil} to={"/SecretariatLive#ancre"}>Accueil</Link>
-                    {/* <Link className={service} onMouseEnter={handleMenu} to={location[3]}>Services</Link> */}
+            <nav  className={`navBar ${showMenu ? "showNav" : "hideNav"}`}>
+                <ul className='aLink' >
+                    <li><div id={`${showMenu ? "slideTop0" : "hideNav"}`}><Link id={`${showMenu ? "slideTop" : "hideNav"}`} className={accueil} onMouseEnter={handleClose} onClick={handleAccueil} to={"/SecretariatLive#ancre"}>Accueil</Link></div></li>
                     
                     {sousMenu === 1 ?
                     <div className='sousMenuContainer'>
-                        <Link className={service} onMouseEnter={handleMenu} to={location[3]}>Services</Link>
+                        <li><div id={`${showMenu ? "slideTop1" : "hideNav"}`}><Link className={service} onMouseEnter={handleMenu} to={location[3]}>Services</Link></div></li>
                         <div onMouseLeave={handleClose} onMouseEnter={handleMenu} className='sousMenu'>
-                            <Link onClick={handleCloseSousMenu} to={"/entreprises#ancre"}>Entreprises</Link>
-                            <Link onClick={handleCloseSousMenu} to={"particuliers#ancre"}>Particuliers</Link>
+                            <li><Link onClick={handleCloseSousMenu} to={"/entreprises#ancre"}>Entreprises</Link></li>
+                            <li><Link onClick={handleCloseSousMenu} to={"particuliers#ancre"}>Particuliers</Link></li>
                         </div>
                     </div>
                         :
-                        <div><Link className={service} onMouseEnter={handleMenu} to={location[3]}>Services</Link></div>
+                        <li><div id={`${showMenu ? "slideTop1" : "hideNav"}`}><Link className={service} onMouseEnter={handleMenu} to={location[3]}>Services</Link></div></li>
                     }
                     
-                    <Link className={tarifs} onMouseEnter={handleClose} onClick={handleTarifs} to={"/tarifs#ancre"}>Tarifs</Link>
-                    <Link className={contact} onMouseEnter={handleClose} onClick={handleContact} to={"/contact#ancre"}>Contact</Link>
-                    <Link className={faq} onMouseEnter={handleClose} onClick={handleFAQ} to={"/FAQ#ancre"}>FAQ</Link>      
+                    <li><div id={`${showMenu ? "slideTop2" : "hideNav"}`}><Link id={`${showMenu ? "slideTop" : "hideNav"}`} className={tarifs} onMouseEnter={handleClose} onClick={handleTarifs} to={"/tarifs#ancre"}>Tarifs</Link></div></li>
+                    <li><div id={`${showMenu ? "slideTop3" : "hideNav"}`}><Link id={`${showMenu ? "slideTop" : "hideNav"}`} className={contact} onMouseEnter={handleClose} onClick={handleContact} to={"/contact#ancre"}>Contact</Link></div></li>
+                    <li><div id={`${showMenu ? "slideTop4" : "hideNav"}`}><Link id={`${showMenu ? "slideTop" : "hideNav"}`} className={faq} onMouseEnter={handleClose} onClick={handleFAQ} to={"/FAQ#ancre"}>FAQ</Link></div></li>      
                 </ul>
                 <button className='navBar_Burger' onClick={handleShow}>
                     <span className='burger_Bar'></span>
