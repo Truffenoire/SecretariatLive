99dfbe0e6abce13f6b829827cf3234d1cb2750fb
GuillaumeM <Guiwilh@gmail.com>
2022-09-26 20:06:47 +0200

    test hashRouter4

--- src/App.jsx
+++ src/App.jsx
@@ -34,13 +34,13 @@ function App() {
       <Header showMenu={showMenu} setShowMenu={setShowMenu} accueil={accueil} setAccueil={setAccueil} service={service} setService={setService} tarifs={tarifs} setTarifs={setTarifs}
         contact={contact} setContact={setContact} faq={faq} setFaq={setFaq} sousMenu={sousMenu} setSousMenu={setSousMenu} />
       <Routes>
-        <Route path="#/SecretariatLive" exact element={<Home />} />
-        <Route path="#/entreprises" exact element={<Entreprises />} />
-        <Route path="#/particuliers" exact element={<Particuliers />} />
-        <Route path="#/tarifs" exact element={<Tarifs />} />
-        <Route path="#/contact" exact element={<Contact />} />
-        <Route path="#/rgpd" exact element={<RGPD />} />
-        <Route path="#/FAQ" exact element={<FAQ setAccueil={setAccueil} setService={setService} setTarifs={setTarifs} setContact={setContact} setFaq={setFaq}/>} />
+        <Route path="/SecretariatLive" exact element={<Home />} />
+        <Route path="/entreprises" exact element={<Entreprises />} />
+        <Route path="/particuliers" exact element={<Particuliers />} />
+        <Route path="/tarifs" exact element={<Tarifs />} />
+        <Route path="/contact" exact element={<Contact />} />
+        <Route path="/rgpd" exact element={<RGPD />} />
+        <Route path="/FAQ" exact element={<FAQ setAccueil={setAccueil} setService={setService} setTarifs={setTarifs} setContact={setContact} setFaq={setFaq}/>} />
       </Routes>
       <Footer setAccueil={setAccueil} setService={setService} setTarifs={setTarifs} setContact={setContact} setFaq={setFaq}/>
 
